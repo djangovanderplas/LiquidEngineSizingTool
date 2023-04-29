f6caef4036436f22f3857624a1edbe45d5c0a710
djangovanderpl <djangovanderplas@minidisco.nl>
2023-04-30 00:24:34 +0200

    Create Propellant.py to hold the CEA class for propellants

---
+++ PropellantSelection/Propellant.py
@@ -0,0 +1,31 @@
+from pycea import CEA, blends
+import yaml
+
+def genCEAObj(fuel, fuel_mass_fraction, oxidizer, oxidizer_mass_fraction):
+    """
+    This function generates a CEA object based on the given propellant and mass fractions.
+    :param fuel:
+    :param fuel_mass_fraction:
+    :param oxidizer:
+    :param oxidizer_mass_fraction:
+    :return:
+    """
+    # Create new blends
+    Fuel = blends.newFuelBlend(fuelL=fuel, fuelPcentL=fuel_mass_fraction)
+    Oxidizer = blends.newOxBlend(oxL=oxidizer, oxPcentL=oxidizer_mass_fraction)
+
+    # Create CEA object
+    cea = CEA(propName='', fuelName=Fuel, oxName=Oxidizer, units="metric")
+    return cea
+
+if __name__ == '__main__':
+    with open('../config.yaml') as f:
+        config = yaml.load(f, Loader=yaml.FullLoader)
+
+    fuel = config['Fuel']
+    fuel_mass_fraction = config['FuelMassFraction']
+    oxidizer = config['Oxidizer']
+    oxidizer_mass_fraction = config['OxidizerMassFraction']
+
+    cea = genCEAObj(fuel, fuel_mass_fraction, oxidizer, oxidizer_mass_fraction)
+    print(cea.get_Isp(Pc=30e5, MR=1.49))
